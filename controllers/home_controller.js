83719fcf9586a6f6335b6ab8875d01add34dfe2c
Akhilesh17-Tech <akhilesh.tech99@gmail.com>
2021-12-29 13:43:52 +0530

    second commit : create route and controller

---
+++ controllers/home_controller.js
@@ -0,0 +1,5 @@
+module.exports.home = function (req, res) {
+  return res.end("<h1>Hello Akhilesh controller home is running fine </h1>");
+};
+
+// module.export.actionName = function(req, res){} // syntax
--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const port = 8000; // default app run on port 80
 const app = express();
 
+// use express router
+app.use("/", require("./routes"));
+
+
+
 app.listen(port, function (err) {
   if (err) {
     console.log(`error in express : ${err}`);
