1eca9681e7f4a33b38a91bf404317ab2bd90f8b9
Akhilesh17-Tech <akhilesh.tech99@gmail.com>
2022-02-23 10:34:03 +0530

    likes functionality created

--- assets/js/home_post_comments.js
+++ assets/js/home_post_comments.js
@@ -35,6 +35,9 @@ class PostComments {
           $(`#post-comments-${postId}`).prepend(newComment);
           pSelf.deleteComment($(" .delete-comment-button", newComment));
 
+          // CHANGE :: enable the functionality of the toggle like button on the new comment
+          new ToggleLike($(" .toggle-like-button", newComment));
+
           new Noty({
             theme: "relax",
             text: "Comment published!",
